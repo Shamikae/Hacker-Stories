9e85ba8df26f1fcea67d34a6c9f873364e059a64
Shamika Earle <e.shamika@yahoo.com>
2020-11-18 06:06:44 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+    const welcome = {
+        greeting: 'Hey',
+        title: 'React',
+    };
+    
+    return (
+    <div>
+        <h1>
+            {welcome.greeting} {welcome.title}
+        </h1>
+
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
