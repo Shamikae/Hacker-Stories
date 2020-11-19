d4a299fcff9e61ff87f2390ef97f6e64a0265448
Shamika Earle <e.shamika@yahoo.com>
2020-11-19 09:08:20 -0500

    HW update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
+import  React from 'react';
 
+function getTitle(title) {
+    return title;
+}
+
+// const welcome = {
+//     greeting: 'Hey',
+//     title: 'React',
+// };
+
+const list = [
+    {
+        title: 'React',
+        url: 'http://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'http://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 
 function App() {
-    const welcome = {
-        greeting: 'Hey',
-        title: 'React',
-    };
-    
     return (
     <div>
         <h1>
-            {welcome.greeting} {welcome.title}
+            My Hacker Stories
+            {/* Hello {getTitle('React')} */}
+            {/* {welcome.greeting} {welcome.title} */}
         </h1>
 
         <label htmlFor="search">Search: </label>
         <input id="search" type="text" />
+
+        <hr/>
+
+        {/* {list.map(function(item) {
+            return ( 
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>                 
+            </div>
+            );
+        })} */}
+        <List/>
+
+        {/* render list */}
     </div>
   );
 }
+
+function List() {
+    return (
+    <div>
+        {list.map(function(item) {
+            return (
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}> {item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                    </div>
+                    );
+                    })}
+                    </div>
+                    );
+                }
 
 export default App;
